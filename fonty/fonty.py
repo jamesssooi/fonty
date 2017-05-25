a7046dc0b418baf2dfecdaf53256a5a1b49e7e47
James Ooi <wengteikooi@gmail.com>
2017-05-25 14:19:40 +0800

    Create basic commands

--- fonty/fonty.py
+++ fonty/fonty.py
@@ -1,7 +1,27 @@
-"""fonty.fonty: entry point for fonty"""
+'''fonty.fonty: entry point for fonty'''
 import click
 
-@click.command()
+
+@click.group()
 def main():
-    """Entry function for fonty"""
-    click.echo('Hello world!')
+    '''Entry function for fonty'''
+    pass
+
+
+@click.command()
+@click.argument('name')
+def install(name):
+    '''Installs a font'''
+    click.echo('Installing font ' + name)
+
+
+@click.command()
+@click.argument('name')
+def uninstall(name):
+    '''Uninstalls a font'''
+    click.echo('Uninstalling font ' + name)
+
+
+# register commands
+main.add_command(install)
+main.add_command(uninstall)
