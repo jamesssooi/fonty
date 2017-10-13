2ce5a68144ea46610366c64eea18119c3c92dc49
James Ooi <wengteikooi@gmail.com>
2017-10-13 23:52:42 +0800

    Remove test command

--- fonty/fonty.py
+++ fonty/fonty.py
@@ -14,26 +14,6 @@ def main():
     '''Fonty 0.1.0'''
     pass
 
-@click.command()
-def test():
-    '''Testing command'''
-    # from fonty.models.manifest import Manifest
-    # manifest = Manifest.load('/Users/jamesooi/Desktop/manifest.json')
-    # typeface = manifest.get('Abel')
-    # print(typeface.uninstall())
-    # from fonty.lib import utils
-    # data = [{'name': 'James Ooi', 'age': 23},
-    #         {'name': 'Louise Ng', 'age': 19}]
-
-    # print(utils.tabularize(data))
-    import os
-    from fonty.models.manifest import Manifest
-    from fonty.models.font import FontFormat
-    manifest = Manifest.load()
-    lato = manifest.get('Lato')
-    lato_regular = lato.get_fonts(['400'])[0]
-    print(lato_regular.parse())
-
 # Register commands
 main.add_command(cli_install)
 main.add_command(cli_uninstall)
