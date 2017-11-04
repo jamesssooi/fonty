d1388e72b8d2f2ff16a3a3b7583bed4044f0ac65
James Ooi <wengteikooi@gmail.com>
2017-11-04 23:11:30 +0800

    Move import into function to prevent importing of win32 modules on OS X

--- fonty/commands/test.py
+++ fonty/commands/test.py
@@ -3,11 +3,11 @@ import os
 
 import click
 from fonty.models.font import Font
-from fonty.lib.install_windows import install_fonts, uninstall_fonts
 
 @click.command('test')
 @click.argument('font_path', type=click.Path())
 def test(font_path):
+    from fonty.lib.install_windows import install_fonts, uninstall_fonts
 
     font_path = 'C:\\Windows\\Fonts\\Lato-Regular.ttf'
 
@@ -15,7 +15,7 @@ def test(font_path):
     with open(font_path, 'rb') as f:
         font_bytes = f.read()
 
-    font = Font(local_path=os.path.abspath(font_path),
+    font = Font(path_to_font=os.path.abspath(font_path),
                 filename=os.path.basename(font_path),
                 raw_bytes=font_bytes)
     
