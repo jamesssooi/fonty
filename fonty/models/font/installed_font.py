659886629028b617f337fc1a199873efd2fe5b7b
James Ooi <wengteikooi@gmail.com>
2017-11-05 00:51:22 +0800

    Update InstalledFont model

--- fonty/models/font/installed_font.py
+++ fonty/models/font/installed_font.py
@@ -3,10 +3,10 @@
 from .font import Font
 
 class InstalledFont(Font):
-    def __init__(self, installed_path: str, registry_path: str = None, **kwargs) -> None:
+    def __init__(self, installed_path: str, registry_name: str = None, **kwargs) -> None:
         super().__init__(path_to_font=installed_path, **kwargs)
         self.installed_path = installed_path
-        self.registry_path = registry_path
+        self.registry_name = registry_name
 
     def uninstall(self) -> bool:
         '''Uninstall this font from the system.'''
