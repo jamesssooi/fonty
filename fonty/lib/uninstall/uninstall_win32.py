71661822212d5889069a64f4572dc8befdb9659b
James Ooi <wengteikooi@gmail.com>
2017-11-05 03:58:44 +0800

    Delete font files using winshell

--- fonty/lib/uninstall/uninstall_win32.py
+++ fonty/lib/uninstall/uninstall_win32.py
@@ -4,6 +4,7 @@ import ctypes
 from typing import List
 
 import winreg
+import winshell
 import win32api
 import win32con
 from fonty.models.font import InstalledFont
@@ -23,26 +24,27 @@ def uninstall_win32(fonts: List[InstalledFont]) -> List[InstalledFont]:
         # from the current session.
         gdi32 = ctypes.WinDLL('gdi32')
         gdi32.RemoveFontResourceW.argtype = (ctypes.c_wchar_p,)
-        gdi32.RemoveFontResourceW(ctypes.c_wchar_p(font.path_to_font))
+        res = gdi32.RemoveFontResourceW(ctypes.c_wchar_p(font.path_to_font))
 
         # Find the registry value name to delete
         registry_value = os.path.basename(font.path_to_font)
         registry_value_name = next((
             val[0] for val in registry_values if val[1] == registry_value
         ), None)
-        print(registry_value_name)
 
         # Then we remove the font from the registry
-        reg_path = 'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts'
-        font_reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path, 0, winreg.KEY_WRITE)
-        winreg.DeleteValue(font_reg, registry_value_name)
+        if registry_value_name:
+            reg_path = 'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts'
+            font_reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path, 0, winreg.KEY_WRITE)
+            winreg.DeleteValue(font_reg, registry_value_name)
 
-        # Then we broadcast a message to all top-level windows that the font
-        # list has changed.
+        # Then we delete the file
+        winshell.delete_file(font.path_to_font)
+        uninstalled_fonts.append(font)
+
+        # And finally, we broadcast a message to all top-level windows that the
+        # font list has changed.
         win32api.PostMessage(win32con.HWND_BROADCAST, win32con.WM_FONTCHANGE)
 
-        # Finally, we remove the font file
-        os.remove(font.path_to_font)
-        uninstalled_fonts.append(font)
 
     return uninstalled_fonts
