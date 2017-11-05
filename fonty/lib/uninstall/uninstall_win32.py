f57b843378f52c3360ea32b232dc4937eeedeeb5
James Ooi <wengteikooi@gmail.com>
2017-11-05 18:54:38 +0800

    Fix buggy uninstallation on Windows
    Literally dying trying to figure this out. Dealing with fonts on Windows
    is an absolute nightmare.
    
    This commit fixes all (so far) unexpected behaviour when it comes to
    uninstalling fonts on Windows. Previously uninstalling fonts might leave
    orphan, unremovable, blank font listings in applications. While they
    usually disappear after reboots, this is obviously quite annoying.
    
    The following changes introduced in this commit arranges the Windows API
    calls in the apparently right specific order (through trial and error).

--- fonty/lib/uninstall/uninstall_win32.py
+++ fonty/lib/uninstall/uninstall_win32.py
@@ -1,50 +1,91 @@
 '''uninstall_win32.py: Uninstall fonts on Windows'''
 import os
+import sys
 import ctypes
+import shutil
+import errno
+from subprocess import Popen
 from typing import List
 
 import winreg
 import winshell
 import win32api
 import win32con
+from win32com.shell import shell, shellcon
 from fonty.models.font import InstalledFont
+from fonty.lib.constants import TMP_DIR
 
 def uninstall_win32(fonts: List[InstalledFont]) -> List[InstalledFont]:
     '''Uninstall fonts on a Windows system'''
     uninstalled_fonts = []
 
-    # Store a copy of the registry in memory
+    # Filter fonts that do not exist
+    for font in fonts:
+        if not os.path.isfile(font.path_to_font):
+            uninstalled_fonts.append(font)
+            fonts.remove(font)
+
+    # Remove all fonts from the session
+    gdi32 = ctypes.WinDLL('gdi32')
+    gdi32.RemoveFontResourceW.argtype = (ctypes.c_wchar_p,)
+    for font in fonts:
+        gdi32.RemoveFontResourceW(ctypes.c_wchar_p(font.path_to_font))
+    
+    # Delete font files
+    deleted_fonts = []
+    flags = shellcon.FOF_WANTMAPPINGHANDLE | shellcon.FOF_ALLOWUNDO | shellcon.FOF_SILENT
+    for font in fonts:
+        # Make a temp copy
+        path_to_copy = os.path.join(TMP_DIR, os.path.basename(font.path_to_font))
+        shutil.copy2(font.path_to_font, path_to_copy)
+
+        # Delete file using shell
+        return_code, aborted, _ = shell.SHFileOperation(
+            (0, shellcon.FO_DELETE, font.path_to_font, '', flags, None, None)
+        )
+
+        if return_code != 0:
+            current_font = font
+            break
+        else:
+            deleted_fonts.append({'font': font, 'copy': path_to_copy })
+
+    # If font deletion fails (file in use, etc), re-add the fonts into the
+    # current session to revert to previous state
+    if return_code != 0:
+        for font in deleted_fonts:
+            winshell.move_file(font['copy'], font['font'].path_to_font)
+        for font in fonts:
+            gdi32.AddFontResourceW(ctypes.c_wchar_p(font.path_to_font))
+        
+        if return_code == 124: # Font file in use
+            raise IOError(
+                errno.EACCES,
+                'File is being used by another process',
+                os.path.basename(current_font.path_to_font)
+            )
+
+    # Remove font from registry
     reg_path = 'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts'
     font_reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path)
     value_count = winreg.QueryInfoKey(font_reg)[1]
     registry_values = [winreg.EnumValue(font_reg, i) for i in range(0, value_count)]
-
     for font in fonts:
-        # Firstly, we call the RemoveFontResource Win32 API to remove the font
-        # from the current session.
-        gdi32 = ctypes.WinDLL('gdi32')
-        gdi32.RemoveFontResourceW.argtype = (ctypes.c_wchar_p,)
-        res = gdi32.RemoveFontResourceW(ctypes.c_wchar_p(font.path_to_font))
-
-        # Find the registry value name to delete
         registry_value = os.path.basename(font.path_to_font)
         registry_value_name = next((
             val[0] for val in registry_values if val[1] == registry_value
         ), None)
 
-        # Then we remove the font from the registry
         if registry_value_name:
             reg_path = 'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts'
             font_reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path, 0, winreg.KEY_WRITE)
             winreg.DeleteValue(font_reg, registry_value_name)
 
-        # Then we delete the file
-        winshell.delete_file(font.path_to_font)
-        uninstalled_fonts.append(font)
+    # Remove temporary files
+    for font in deleted_fonts:
+        winshell.delete_file(font['copy'])
 
-        # And finally, we broadcast a message to all top-level windows that the
-        # font list has changed.
-        win32api.PostMessage(win32con.HWND_BROADCAST, win32con.WM_FONTCHANGE)
+    # Broadcast message to all top-level windows that the font list has changed.
+    win32api.PostMessage(win32con.HWND_BROADCAST, win32con.WM_FONTCHANGE)
 
-
-    return uninstalled_fonts
+    return fonts
