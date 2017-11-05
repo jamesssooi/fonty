6073946f023815d048fc3120878a4930c45a0608
James Ooi <wengteikooi@gmail.com>
2017-11-05 20:40:11 +0800

    Remove unnecessary import

--- fonty/fonty.py
+++ fonty/fonty.py
@@ -40,7 +40,6 @@ def main(ctx, version: bool):
       Create webfonts of an existing installed font from this computer:
       >>> fonty webfont --typeface "Open Sans"
     '''
-    from fonty.lib.constants import ROOT_DIR
     if ctx.invoked_subcommand:
         return
 
