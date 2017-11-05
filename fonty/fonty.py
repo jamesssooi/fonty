f7272de73244806a900d6e085922fdc044a923c0
James Ooi <wengteikooi@gmail.com>
2017-11-05 20:36:21 +0800

    Remove test command

--- fonty/fonty.py
+++ fonty/fonty.py
@@ -13,7 +13,6 @@ from fonty.commands.uninstall import cli_uninstall
 from fonty.commands.source import cli_source
 from fonty.commands.list import cli_list
 from fonty.commands.webfont import cli_webfont
-from fonty.commands.test import test
 
 # Enable colored output on Windows
 colorama.init()
@@ -63,4 +62,3 @@ main.add_command(cli_uninstall)
 main.add_command(cli_source)
 main.add_command(cli_list)
 main.add_command(cli_webfont)
-main.add_command(test)
