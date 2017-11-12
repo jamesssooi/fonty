8081c485dede408a5363e2d738ec6fe8eb889c07
James Ooi <wengteikooi@gmail.com>
2017-11-12 23:13:12 +0800

    Remove unused download method in RemoteFont model

--- fonty/models/font/remote_font.py
+++ fonty/models/font/remote_font.py
@@ -99,37 +99,3 @@ class RemoteFont(object):
                 iterator.close()
 
         return Font(path_to_font=path_to_font)
-
-
-    def download(self, path: str = None, handler = None):
-        '''Download this font into a tmp directory and return a Font instance.'''
-        from .font import Font
-
-        request = requests.get(self.remote_path.path, stream=True)
-        if handler:
-            iterator = handler(self, request)
-            next(iterator)
-
-        total_bytes = b''
-        for bytes_ in request.iter_content(128):
-            if bytes_:
-                total_bytes += bytes_
-
-                # Send total bytes downloaded to the handler. We use
-                # `request.raw.tell()` instead of `len(bytes_)` to
-                # account for requests with gzip compression.
-                if handler:
-                    iterator.send(request.raw.tell()) # total bytes received
-
-        # Save font to directory if provided, or to a tmp directory if not
-        output_folder = path if path else TMP_DIR
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder, exist_ok=True)
-        path_to_font = os.path.join(output_folder, self.filename)
-
-        with open(path_to_font, 'wb+') as f:
-            f.write(total_bytes)
-
-        self._tmp_path = path_to_font
-
-        return Font(path_to_font=path_to_font)
