c13d9c1b05206f9a000d9fb7a0e6733e543ecf9e
James Ooi <wengteikooi@gmail.com>
2018-03-24 18:03:58 +0800

    Implement initial manifest and config generation

--- fonty/setup.py
+++ fonty/setup.py
@@ -1,25 +1,48 @@
 '''fonty.setup: initial setup script for fonty.'''
 import os
 import json
+import shutil
 
+import click
+import ansiwrap
 from termcolor import colored
-from fonty.lib.constants import ROOT_DIR, COLOR_INPUT
+from fonty.lib.constants import APP_DIR, ROOT_DIR, COLOR_INPUT
 from fonty.lib.task import Task
 from fonty.models.subscription import Subscription, AlreadySubscribedError
+from fonty.models.manifest import Manifest
 from fonty.lib import search
 
-def setup():
+def initial_setup() -> None:
+
+    # Print welcome message
+    initial_setup_message = '\n'.join([
+        "Looks like this is your first time running fonty! Running initial setup scripts...",
+    ])
+    click.echo(initial_setup_message)
 
     # Subscribe to default sources
     generate_default_subscriptions()
 
-def generate_config_file():
-    pass
+    # Generate initial font manifest
+    generate_manifest()
 
-def generate_manifest():
-    pass
+    # Generate initial config file
+    generate_config()
 
-def generate_default_subscriptions():
+    # Print new line
+    click.echo("")
+
+def generate_manifest() -> None:
+    task = Task("Generating initial font manifest...")
+
+    # Generate new manifest
+    manifest = Manifest.generate()
+    manifest.save()
+
+    # Done!
+    task.complete()
+
+def generate_default_subscriptions() -> None:
 
     # Get list of default sources
     path_to_defaults = os.path.join(ROOT_DIR, 'defaults', 'sources.json')
@@ -29,24 +52,35 @@ def generate_default_subscriptions():
 
     # Subscribe to each default sources
     for source in sources:
-        task = Task("Loading '{}'...".format(colored(source['name'], COLOR_INPUT)))
+        task = Task("Subscribing to default source '{}'...".format(colored(source['name'], COLOR_INPUT)))
 
         # Subscribe to source
         sub = Subscription.load_from_url(source['url'])
         try:
             sub.subscribe()
-        except AlreadySubscribedError as e:
+        except AlreadySubscribedError:
             sub = sub.get(sub.id_)
 
         # Index fonts
         repo = sub.get_local_repository()
-        task.message = "Indexing {count} font families in '{repo}'".format(
-            count=len(repo.families),
-            repo=colored(repo.name, COLOR_INPUT)
-        )
         search.index_fonts(repo, sub.local_path)
 
-        task.complete("Indexed {count} new font families in '{repo}'".format(
-            count=len(repo.families),
-            repo=colored(repo.name, COLOR_INPUT)
-        ))
+        # Done!
+        task.complete()
+
+def generate_config() -> None:
+    '''Generate default fonty configuration file.'''
+    task = Task("Generating default configuration file...")
+
+    # Copy default configuration file to app directory
+    path_to_default = os.path.join(ROOT_DIR, 'defaults', 'fonty.conf')
+    dest_path = os.path.join(APP_DIR, 'fonty.conf')
+    shutil.copyfile(path_to_default, dest_path)
+
+    # Done!
+    task.complete()
+
+def is_first_run() -> bool:
+    '''Check if fonty has already been setup on this system.'''
+    path_to_config = os.path.join(APP_DIR, 'fonty.conf')
+    return not os.path.isfile(path_to_config)
