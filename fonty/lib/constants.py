5097a084b89f056f2285db2be98133ffee854212
James Ooi <wengteikooi@gmail.com>
2018-05-01 17:36:16 +0800

    Update telemetry endpoint

--- fonty/lib/constants.py
+++ fonty/lib/constants.py
@@ -20,8 +20,7 @@ REPOSITORY_DIR = os.path.join(APP_DIR, 'repositories')
 CONFIG_FILENAME = 'fonty.conf'
 
 # URLs
-TELEMETRY_ENDPOINT = 'http://127.0.0.1:5000'
-# TELEMETRY_ENDPOINT = 'https://analytics.fonty.io/v1'
+TELEMETRY_ENDPOINT = 'https://analytics.fonty.io/v1'
 
 # Colors
 COLOR_OK = 'green'
